0aa9ba139db54e81299920b7a06cff994153fff4
camael24 <thehawk@hoa-project.net>
2015-04-21 14:17:47 +0200

    Update

--- Application/View/Uia/User/Edit.tpl.php
+++ Application/View/Uia/User/Edit.tpl.php
@@ -16,24 +16,24 @@ $this->block('container');
         <section id="contenu">
             <form action="<?php echo $this->route->unroute('updateUiaUser', ['user_id' => $profil->getId()]); ?>"
                   method="post" enctype="application/x-www-form-urlencoded">
-                <div id="questions">
+                <section>
                     <h4><?php echo $profil->getRealName(); ?></h4>
-                </div>
-                <div><label for="name">RealName : </label><input type="text" name="name" id="name" value="<?php echo $profil->getRealName(); ?>" /></div>
-                <div><label for="login">Login : </label><input type="text" name="login" id="login" value="<?php echo $profil->getLogin(); ?>" /></div>
-                <div><label for="email">Email : </label><input type="email" name="email" id="email" value="<?php echo $profil->getEmail(); ?>" /></div>
-                <div><label for="email">Birthdate : </label><input type="date" name="birthdate" id="birthdate" value="<?php echo date('H:i:s d/m/Y', $profil->getBirthdate()); ?>" /></div>
-                <section id="changepsswd" class="col">
+                    <div><label for="name">RealName : </label><input type="text" name="name" id="name" value="<?php echo $profil->getRealName(); ?>" /></div>
+                    <div><label for="login">Login : </label><input type="text" name="login" id="login" value="<?php echo $profil->getLogin(); ?>" /></div>
+                    <div><label for="email">Email : </label><input type="email" name="email" id="email" value="<?php echo $profil->getEmail(); ?>" /></div>
+                    <div><label for="email">Birthdate : </label><input type="date" name="birthdate" id="birthdate" value="<?php echo date('H:i:s d/m/Y', $profil->getBirthdate()); ?>" /></div>
+                </section>
+                <section id="changepsswd">
                     <h4>Gestion des mots de passe</h4>
                     <div><label for="psswd">Nouveau Mot de passe : </label><input type="password" name="psswd" id="psswd"/></div>
                     <div><label for="cpsswd">Confirmation Mot de passe : </label><input type="password" name="cpsswd" id="cpsswd"/></div>
                 </section>
-                <section id="adminZone" class="col">
+                <section id="adminZone">
                     <h4>Gestion des droits</h4>
                     <div><label for="isAdmin">Administrator : </label><input type="checkbox" name="isAdmin" id="isAdmin" <?php echo ($profil->getIsAdmin() === true) ? 'checked' : ''; ?>/></div>
                     <div><label for="isModo">Moderator : </label><input type="checkbox" name="isModo" id="isModo" <?php echo ($profil->getIsModerator() === true) ? 'checked' : ''; ?>/></div>
                     <div><label for="isProf">Professor : </label><input type="checkbox" name="isProf" id="isProf" <?php echo ($profil->getIsProfessor() === true) ? 'checked' : ''; ?>/></div>
-                    <div><label>Status :</label>   <?php
+                    <div><label>Status :</label> <span><?php
                             switch($profil->getStatus())
                             {
                                 case -1:
@@ -51,7 +51,7 @@ $this->block('container');
                                 default:
                                     echo '#ERROR CALL AN ADMINISTRATOR#';
                             }
-                        ?></div>
+                        ?></span></div>
                 </section>
                 <input type="submit" value="ENREGISTRER" />
             </form>
--- Application/View/base.tpl.php
+++ Application/View/base.tpl.php
@@ -1,16 +1,9 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
     "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<head>
-    <title>Maestria</title>
-    <link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
-    <link href="/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/css/maestria.css" rel="stylesheet" type="text/css">
-    <meta charset="UTF-8"/>
-</head>
 <body>
 <header>
-    <a href="/">Maestria</a> Evaluer c'est connaître et comprendre c'est progresser
+    Evaluation multicritère graphisée
     <?php if (isset($userIsLogin) and $userIsLogin === true) { ?>
         <div id="logzone">
             <?php
@@ -29,29 +22,27 @@
     <?php } ?>
 </header>
 <nav>
+    <a href="/" class="logo"><img src="/img/maestria.jpg" alt="logo maestria"/></a>
     <h3><a href="<?php echo $this->route->unroute('indexUiaClassroom'); ?>">CLASSES</a></h3>
-
-    <h3 class="synthese"><a href="/">SYNTHESE</a></h3>
+    <h3 class="synthese"><a href="synthese.html">SYNTHESE</a></h3>
     <br/>
-
-    <h3><a href="/">EVALUATIONS</a></h3>
-
+    <h3><a href="evals.html">EVALUATIONS</a></h3>
     <h3 id="evalchx" class="eval">PUISSANCE</h3>
 
     <div class="flechebas"></div>
     <h3 class="eval"><a href="/">CORRECTION</a></h3>
-
     <?php if (isset($user) === true && $user->getIsAdmin() === true) { ?>
         <br/>
-
         <h3><a href="<?php echo $this->route->unroute('indexUiaItem'); ?>">ITEMS PEDA</a></h3>
 
     <?php } ?>
+
     <footer>
-        <a href="">Contact</a>
-        <a href="http://metaphysik.fr">Metaphysik</a>
+        <a href="http://metaphysik.fr/manuel/projet.php#contact">Contact</a>|
+        <a href="metaphysik.fr">Metaphysik</a>
     </footer>
 </nav>
+
 <section id="popup">
     <section id="inpopup">
 
@@ -65,4 +56,11 @@
 <?php $this->block('js:script'); ?>
 <?php $this->endBlock(); ?>
 </body>
+<head>
+    <title>Maestria</title>
+    <link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
+    <link href="/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/css/maestria.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8"/>
+</head>
 </html>
