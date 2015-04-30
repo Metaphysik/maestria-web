bb490b2784201bbeaf66d35303007e256dc34d7c
camael24 <thehawk@hoa-project.net>
2015-04-30 12:24:23 +0200

    Fix prod

--- Public/index.php
+++ Public/index.php
@@ -15,7 +15,7 @@ namespace {
 
     // Fix nginx
     $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
-    var_dump(resolve('hoa://Application/'));
+    var_dump(resolve('hoa://Application/View'));
     var_dump(resolve('hoa://Application/', true));
 
     try {
