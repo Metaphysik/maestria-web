fff18f9b6828b0f8d171e482d06272ca5dfcb85b
camael24 <thehawk@hoa-project.net>
2015-04-30 12:31:37 +0200

    Fix prod

--- Application/Maestria/Greut.php
+++ Application/Maestria/Greut.php
@@ -22,7 +22,7 @@ class Greut extends \Sohoa\Framework\View\Greut
             $filename = $this->_paths.$filename;
         }
 
-        var_dump($filename);
+        $filename = str_replace('\\' , '/', $filename);
 
         $resolve = resolve($filename, false);
         $realpath = realpath($resolve); // We need to use resolve beacause realpath dont use stream wrapper
