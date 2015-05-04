25e29bafa3ef885e811c958f1c7c868114753c67
camael24 <thehawk@hoa-project.net>
2015-05-04 12:55:56 +0200

    Template Evaluation/New

--- Application/View/Uia/Evaluation/New.tpl.php
+++ Application/View/Uia/Evaluation/New.tpl.php
@@ -18,55 +18,45 @@ $this->block('container');
                 <div class="titre">
                     <h4 style="margin-top: -8px;">INFOS</h4>
                     <label for="title" class="titrant">TITRE</label>
-                    <input name="title" id="title" value="Découpage">
+                    <input name="title" id="title" value="">
                     <label for="date">DATE</label>
-                    <input name="date" id="date" value="1984-04-20" type="date">
+                    <input name="date" id="date" value="<?php echo date('d-m-Y'); ?>" type="date">
                 </div>
                 <div id="questions"><h4>QUESTIONS</h4></div>
-                <section class="float">
-                    <div><label for="q1_title">TITRE</label><input kl_virtual_keyboard_secure_input="on" name="q1_title"
-                                                                   id="q1_title" placeholder="Titre de la question 1"
-                                                                   value="Nommer les ciseaux"></div>
-                    <div><label for="q1_taxo">NIVEAU TAXONOMIQUE</label><select name="q1_taxo" id="q1_taxo">
-                            <option value="1" undefined="">Connaissance</option>
-                            <option value="2">Compréhension</option>
-                            <option value="3">Application</option>
-                            <option value="4">Analyse</option>
-                        </select></div>
-                    <div><label for="q1_note">POINTS</label><input name="q1_note" id="q1_note" min="0" placeholder="0"
-                                                                   value="2" type="number"></div>
-                    <div><label for="q1_item1"><span class="awsm"> </span>CONNAISSANCE</label><input
-                            kl_virtual_keyboard_secure_input="on" name="q1_item1" id="q1_item1"
-                            placeholder="A choisir parmi les items pédagogiques"
-                            value="Savoir qu'on parle d'une paire de ciseaux"><span class="awsm chitem"></span></div>
-                    <div><label for="q1_item2"><span class="awsm"> </span>SAVOIR-FAIRE OU ATTITUDE</label><input
-                            kl_virtual_keyboard_secure_input="on" name="q1_item2" id="q1_item2"
-                            placeholder="A choisir parmi les items pédagogiques"
-                            value="Savoir nommer les objets de la classe"><span class="awsm chitem"></span></div>
-                </section>
-                <section class="float">
-                    <div><label for="q1_title">TITRE</label><input kl_virtual_keyboard_secure_input="on" name="q1_title"
-                                                                   id="q1_title" placeholder="Titre de la question 1"
-                                                                   value="Nommer les ciseaux"></div>
-                    <div><label for="q1_taxo">NIVEAU TAXONOMIQUE</label><select name="q1_taxo" id="q1_taxo">
-                            <option value="1" undefined="">Connaissance</option>
-                            <option value="2">Compréhension</option>
-                            <option value="3">Application</option>
-                            <option value="4">Analyse</option>
-                        </select></div>
-                    <div><label for="q1_note">POINTS</label><input name="q1_note" id="q1_note" min="0" placeholder="0"
-                                                                   value="2" type="number"></div>
-                    <div><label for="q1_item1"><span class="awsm"> </span>CONNAISSANCE</label><input
-                            kl_virtual_keyboard_secure_input="on" name="q1_item1" id="q1_item1"
-                            placeholder="A choisir parmi les items pédagogiques"
-                            value="Savoir qu'on parle d'une paire de ciseaux"><span class="awsm chitem"></span></div>
-                    <div><label for="q1_item2"><span class="awsm"> </span>SAVOIR-FAIRE OU ATTITUDE</label><input
-                            kl_virtual_keyboard_secure_input="on" name="q1_item2" id="q1_item2"
-                            placeholder="A choisir parmi les items pédagogiques"
-                            value="Savoir nommer les objets de la classe"><span class="awsm chitem"></span></div>
-                </section>
-                <div class="add"><span class="awsm" title="Ajouter évaluation"> </span> AJOUTER QUESTION</div>
-
+                <?php for($i = 1; $i <= 2; $i++) { ?>
+                    <section class="float">
+                        <div>
+                            <label for="q<?php echo $i; ?>_title">TITRE</label>
+                            <input name="q<?php echo $i; ?>_title" id="q<?php echo $i; ?>_title" placeholder="Titre de la question <?php echo $i; ?>"/>
+                        </div>
+                        <div>
+                            <label for="q<?php echo $i; ?>_taxo">NIVEAU TAXONOMIQUE</label>
+                            <select name="q<?php echo $i; ?>_taxo" id="q<?php echo $i; ?>_taxo">
+                                <option value="1">Connaissance</option>
+                                <option value="2">Compréhension</option>
+                                <option value="3">Application</option>
+                                <option value="4">Analyse</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="q<?php echo $i; ?>_note">POINTS</label>
+                            <input name="q<?php echo $i; ?>_note" id="q<?php echo $i; ?>_note" min="0" max="100" placeholder="0"
+                                   type="number"/>
+                        </div>
+                        <div>
+                            <label for="q<?php echo $i; ?>_item1"><i class="awsm fa fa-graduation-cap"></i> CONNAISSANCE</label>
+                            <input name="q<?php echo $i; ?>_item1" id="q<?php echo $i; ?>_item1" placeholder="A choisir parmi les items pédagogiques">
+                            <i class="awsm fa fa-briefcase chitem"></i>
+                        </div>
+                        <div>
+                            <label for="q<?php echo $i; ?>_item2"><i class="awsm fa fa-cogs"></i> SAVOIR-FAIRE OU ATTITUDE</label>
+                            <input name="q<?php echo $i; ?>_item2" id="q<?php echo $i; ?>_item2" placeholder="A choisir parmi les items pédagogiques">
+                            <i class="awsm fa fa-briefcase chitem"></i>
+                        </div>
+                    </section>
+                <?php } ?>
+                <div class="add"><i class="awsm fa fa-plus-square" title="Ajouter évaluation"></i> AJOUTER QUESTION
+                </div>
                 <input id="creer" value="ENREGISTRER" type="submit">
             </form>
         </section>
