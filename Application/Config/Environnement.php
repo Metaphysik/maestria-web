28ddfcd5772f21b9c1b832a926b18e602cae5ef5
camael24 <thehawk@hoa-project.net>
2015-05-07 15:29:23 +0200

    Use Europe/paris timezone

--- Application/Config/Environnement.php
+++ Application/Config/Environnement.php
@@ -5,6 +5,8 @@
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
 
+date_default_timezone_set('Europe/Paris');
+
 $isDevMode     = true;
 $config        = Setup::createAnnotationMetadataConfiguration([__DIR__ . "/../Entities"], $isDevMode);
 $path          = resolve(__DIR__ . '/../Database/Maestria-orm.db');
