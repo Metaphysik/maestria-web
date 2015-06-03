68c4701db2cb6850e99ea07f78101989b361e904
camael24 <thehawk@hoa-project.net>
2015-06-03 17:02:52 +0200

    Start evaluate an student

---
+++ Application/Controller/Uia/Correction.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Application\Controller\Uia;
+
+
+use Application\Controller\Api;
+
+class Correction extends Api {
+
+    public function indexAction($uia) {
+        $this->redirector->redirect('mainindex', ['uia' => $uia]);
+    }
+
+    public function showAction($correction_id) {
+
+        
+
+
+    }
+
+}
--- Application/View/Main/Index.tpl.php
+++ Application/View/Main/Index.tpl.php
@@ -6,6 +6,158 @@
 $this->inherits('hoa://Application/View/base.tpl.php');
 $this->block('container');
 ?>
-    <h1>INDEXXXXXXXXXX</h1>
+    <section id="corps" class="evaluer">
+        <section id="titre">
+            <h3 class="classe">CLASSE <span class="awsm">  </span> <span id="classe">3<sup>2</sup></span></h3>
+
+            <h1>EVALUER <span class="awsm">  </span><span id="evalchx"> PUISSANCE</span></h1>
+
+        </section>
+        <section id="contenu">
+            <div class="boutons">
+                <h4 class="optaff">OPTIONS D'AFFICHAGE</h4>
+            </div>
+            <section style="width: 1160px;" id="cases">
+                <article draggable="true" data-idelv="3201">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Nicolass</div>
+                    <div class="awsm taxo" style="color:rgb(51,255,0)"><span></span><span
+                            style="color:rgb(0,255,0)"></span><span style="color:rgb(255,204,0)"></span><span
+                            style="color:rgb(0,255,0)"></span></div>
+                    <div class="prctg">60</div>
+                </article>
+                <article draggable="true" data-idelv="3202">
+                    <div class="awsm perso" style="color:rgb(255,102,0)"></div>
+                    <div class="nom">Allann Wauters de Besterfeld</div>
+                    <div class="awsm taxo" style="color:rgb(102,255,0)"><span></span><span style="color:rgb(51,255,0)"></span><span
+                            style="color:rgb(255,153,0)"></span><span style="color:rgb(26,255,0)"></span></div>
+                    <div class="prctg">50</div>
+                </article>
+                <article draggable="true" data-idelv="3203">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Nanana Cerise</div>
+                    <div class="awsm taxo" style="color:rgb(51,255,0)"><span></span><span
+                            style="color:rgb(0,255,0)"></span><span style="color:rgb(255,204,0)"></span><span
+                            style="color:rgb(0,255,0)"></span></div>
+                    <div class="prctg">60</div>
+                </article>
+                <article draggable="true" data-idelv="3204">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Bertrand</div>
+                    <div class="awsm taxo" style="color:rgb(255,51,0)"><span></span><span style="color:rgb(255,102,0)"></span><span
+                            style="color:rgb(255,153,0)"></span><span style="color:rgb(255,204,0)"></span></div>
+                    <div class="prctg">60</div>
+                </article>
+                <article draggable="true" data-idelv="3205">
+                    <div class="awsm perso" style="color:rgb(255,102,0)"></div>
+                    <div class="nom">Alizé</div>
+                    <div class="awsm taxo" style="color:rgb(255,255,0)"><span></span><span
+                            style="color:rgb(204,255,0)"></span><span style="color:rgb(153,255,0)"></span><span
+                            style="color:rgb(102,255,0)"></span></div>
+                    <div class="prctg">50</div>
+                </article>
+                <article draggable="true" data-idelv="3206">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Massilia</div>
+                    <div class="awsm taxo" style="color:rgb(51,255,0)"><span></span><span
+                            style="color:rgb(0,255,0)"></span><span style="color:rgb(255,204,0)"></span><span
+                            style="color:rgb(0,255,0)"></span></div>
+                    <div class="prctg">60</div>
+                </article>
+                <article draggable="true" data-idelv="3207">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Ernest</div>
+                    <div class="awsm taxo" style="color:rgb(51,255,0)"><span></span><span
+                            style="color:rgb(0,255,0)"></span><span style="color:rgb(255,204,0)"></span><span
+                            style="color:rgb(0,255,0)"></span></div>
+                    <div class="prctg">60</div>
+                </article>
+                <article draggable="true" data-idelv="3209">
+                    <div class="awsm perso" style="color:rgb(255,102,0)"></div>
+                    <div class="nom">Biroute</div>
+                    <div class="awsm taxo" style="color:rgb(102,255,0)"><span></span><span style="color:rgb(51,255,0)"></span><span
+                            style="color:rgb(255,153,0)"></span><span style="color:rgb(26,255,0)"></span></div>
+                    <div class="prctg">50</div>
+                </article>
+                <article draggable="true" data-idelv="3210">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Julien</div>
+                    <div class="awsm taxo" style="color:rgb(51,255,0)"><span></span><span
+                            style="color:rgb(0,255,0)"></span><span style="color:rgb(255,204,0)"></span><span
+                            style="color:rgb(0,255,0)"></span></div>
+                    <div class="prctg">60</div>
+                </article>
+                <article draggable="true" data-idelv="3211">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Maxime</div>
+                    <div class="awsm taxo" style="color:rgb(51,255,0)"><span></span><span
+                            style="color:rgb(0,255,0)"></span><span style="color:rgb(255,204,0)"></span><span
+                            style="color:rgb(0,255,0)"></span></div>
+                    <div class="prctg">60</div>
+                </article>
+                <article draggable="true" data-idelv="3212">
+                    <div class="awsm perso" style="color:rgb(255,102,0)"></div>
+                    <div class="nom">Laurent</div>
+                    <div class="awsm taxo" style="color:rgb(102,255,0)"><span></span><span style="color:rgb(51,255,0)"></span><span
+                            style="color:rgb(255,153,0)"></span><span style="color:rgb(26,255,0)"></span></div>
+                    <div class="prctg">50</div>
+                </article>
+                <article draggable="true" data-idelv="3213">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Catherine</div>
+                    <div class="awsm taxo" style="color:rgb(51,255,0)"><span></span><span
+                            style="color:rgb(0,255,0)"></span><span style="color:rgb(255,204,0)"></span><span
+                            style="color:rgb(0,255,0)"></span></div>
+                    <div class="prctg">60</div>
+                </article>
+                <article draggable="true" data-idelv="3214">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Le Suisse</div>
+                    <div class="awsm taxo" style="color:rgb(255,0,0)"><span></span><span
+                            style="color:rgb(255,102,0)"></span><span style="color:rgb(255,153,0)"></span><span
+                            style="color:rgb(255,204,0)"></span></div>
+                    <div class="prctg">20</div>
+                </article>
+                <article draggable="true" data-idelv="3215">
+                    <div class="awsm perso" style="color:rgb(255,102,0)"></div>
+                    <div class="nom">Berthe aux grands pieds</div>
+                    <div class="awsm taxo" style="color:rgb(102,255,0)"><span></span><span style="color:rgb(51,255,0)"></span><span
+                            style="color:rgb(255,153,0)"></span><span style="color:rgb(26,255,0)"></span></div>
+                    <div class="prctg">50</div>
+                </article>
+                <article draggable="true" data-idelv="3216">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Bruno</div>
+                    <div class="awsm taxo" style="color:rgb(51,255,0)"><span></span><span
+                            style="color:rgb(0,255,0)"></span><span style="color:rgb(255,204,0)"></span><span
+                            style="color:rgb(0,255,0)"></span></div>
+                    <div class="prctg">60</div>
+                </article>
+                <article draggable="true" data-idelv="3217">
+                    <div class="awsm perso" style="color:rgb(255,41,0)"></div>
+                    <div class="nom">Phillipe</div>
+                    <div class="awsm taxo" style="color:rgb(255,10,0)"><span></span><span
+                            style="color:rgb(255,15,0)"></span><span style="color:rgb(255,20,0)"></span><span
+                            style="color:rgb(255,26,0)"></span></div>
+                    <div class="prctg">10</div>
+                </article>
+                <article draggable="true" data-idelv="3218">
+                    <div class="awsm perso" style="color:rgb(255,102,0)"></div>
+                    <div class="nom">Kuntakinté</div>
+                    <div class="awsm taxo" style="color:rgb(102,255,0)"><span></span><span style="color:rgb(51,255,0)"></span><span
+                            style="color:rgb(255,153,0)"></span><span style="color:rgb(26,255,0)"></span></div>
+                    <div class="prctg">50</div>
+                </article>
+                <article draggable="true" data-idelv="3219">
+                    <div class="awsm perso" style="color:rgb(255,153,0)"></div>
+                    <div class="nom">Zoubida</div>
+                    <div class="awsm taxo" style="color:rgb(51,255,0)"><span></span><span
+                            style="color:rgb(0,255,0)"></span><span style="color:rgb(255,204,0)"></span><span
+                            style="color:rgb(0,255,0)"></span></div>
+                    <div class="prctg">60</div>
+                </article>
+            </section>
+        </section>
+    </section>
 <?php
 $this->endblock();
