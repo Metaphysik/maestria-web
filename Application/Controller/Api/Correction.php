63876ca2b01aa9ae8969b32c5e45b276ffa864ae
camael24 <thehawk@hoa-project.net>
2015-09-15 22:05:35 +0200

    etablissement de l'algo de notation

--- Application/Controller/Api/Correction.php
+++ Application/Controller/Api/Correction.php
@@ -5,6 +5,7 @@ namespace Application\Controller\Api;
 use Application\Controller\Api as _Api;
 use Application\Model\Answer;
 use Application\Model\Classroom;
+use Application\Model\Question;
 use Application\Model\User;
 use Application\Model\UserClass;
 
@@ -24,6 +25,32 @@ class Correction extends _Api
         $users    = $m_class->getAllBy('refClassroom', $classe);
         $data     = [];
 
+        if (empty($answers) === false) {
+            $answer = $answers[0];
+            $answers = json_decode($answer->getAnswer(), true);
+
+            foreach ($answers as $q => $note) {
+                $sort_answers[$q] = $note;
+            }
+        }
+
+        foreach ($question_iterator as $question) {
+            /*
+             * @var $question \Application\Entities\Question
+             */
+
+            $a[] = [
+                'id' => $question->getId(),
+                'title' => $question->getTitle(),
+                'taxo' => $question->getTaxo(),
+                // Un int ?
+                'item1' => $question->getItem1(),
+                'item2' => $question->getItem2(),
+                'note' => $question->getPoint(),
+                'current' => (isset($sort_answers[$question->getId()])) ? $sort_answers[$question->getId()] : -1,
+                // -1 Non rep, 0=C , 1=B, 2=A
+            ];
+        }
         // var_dump($users);
 
         if (count($users) > 0) {
@@ -61,5 +88,20 @@ class Correction extends _Api
         }
 
         echo $this->getApiJson();
+
+
+        /**
+         * Dans une lib à part
+         * Recherche les réponses à l'éval pour chaque éléves
+         * Calculer la note (brut et avec le coefficient)
+         * Trié les réponses par taxo (Comprendre, Analyser)
+         * Calculer les notes correspondantes
+         * __construct($eval_id)
+         * getRawNote
+         * getCoeffNote
+         * getItemsRawNote()
+         * getItemsCoeffNote()
+         * getItems()
+         */
     }
 }
