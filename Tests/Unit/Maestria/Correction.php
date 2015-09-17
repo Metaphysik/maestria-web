a790c9d4c9e18ec507ecbf8610d8e2e3bccc4dd3
camael24 <thehawk@hoa-project.net>
2015-09-17 20:33:58 +0200

    test are not ready yet

--- Tests/Unit/Maestria/Correction.php
+++ Tests/Unit/Maestria/Correction.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit\Application\Maestria\Answer;
 
+use Application\Model\Answer;
+use Application\Model\Question;
 use Application\Maestria\Answer\Provider;
 
 class Correction extends \atoum\test
@@ -9,50 +11,24 @@ class Correction extends \atoum\test
     public function testFoo()
     {
         // Classe 1�S
-        $uia = 1;
-        $user = 27;
-        $eval = 7;
-
-        $questions = new \mock\Application\Model\Question();
-        $answer = new \mock\Application\Model\Answer();
-
-        $questions->getMockController();
-        $questions->getByEvaluation = function () {
-            return 'fpp';
-        };
-
-        var_dump($questions->getByEvaluation());
-
-        $provider = new Provider();
-        $provider->setQuestions($questions->getByEvaluation($eval));
-        $provider->setAnswers($answer->getAnswer($uia, $user, $eval));
-
-        $correction = new \Application\Maestria\Answer\Correction();
-        $correction->setProvider($provider);
-
-//        var_dump($correction->getGlobalNote());
-//        var_dump($correction->getNoteItem());
-        var_dump($correction->getNoteTaxo(true));
-
-
-
-
-    }
-    public function testFoobar()
-    {
-//        // Classe 1�S
 //        $uia = 1;
 //        $user = 27;
 //        $eval = 7;
 //
 //        $questions = new Question();
 //        $answer = new Answer();
-//        $items = new Item();
+//
+//        $provider = new Provider();
+//        $provider->setQuestions($questions->getByEvaluation($eval));
+//        $provider->setAnswers($answer->getAnswer($uia, $user, $eval));
 //
 //        $correction = new \Application\Maestria\Answer\Correction();
-//        $correction->setQuestions($questions->getByEvaluation($eval));
-//        $correction->setAnswers($answer->getAnswer($uia, $user, $eval));
-//        $correction->setItems($items->all());
+//        $correction->setProvider($provider);
+//
+//        var_dump($correction->getGlobalNote());
+//        var_dump($correction->getNoteItem());
+//        var_dump($correction->getNoteTaxo(true));
+
 
 
 
